9408ea05cb45719658cf3759170dc05ad044f911
Anon U <Anon U@DESKTOP-I5PLASG>
2024-02-06 12:29:22 -0500

    a rough start

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -11,12 +11,7 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            CounterBtn.Text = "nahhh";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
