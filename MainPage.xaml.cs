7a2e2ef1cfc5c31980caff52f96e0343608aacc6
Ineedsleep <Ineedsleep@DESKTOP-58M2OF2>
2024-02-09 10:42:14 -0500

    icons

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,22 +1,14 @@
-﻿namespace KTI_Testing__Mobile_
+﻿using KTI_Testing__Mobile_.pages;
+using Camera.MAUI;
+using Microsoft.Extensions.Logging;
+
+namespace KTI_Testing__Mobile_
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
-        }
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-             
-
-
-            AddBtn.Text = " +1 ";
-            SubBtn.Text = " -1 ";
-
-
-            SemanticScreenReader.Announce(AddBtn.Text);
-            SemanticScreenReader.Announce(SubBtn.Text);
         }
     }
 
