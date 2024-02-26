adfd68d7fdc1e3fffee8235d517b13cce7282885
Ineedsleep <Ineedsleep@DESKTOP-58M2OF2>
2024-02-26 12:54:04 -0500

    working on Login

--- pages/Login.xaml.cs
+++ pages/Login.xaml.cs
@@ -2,8 +2,14 @@
 namespace KTI_Testing__Mobile_.pages;
 public partial class Login : ContentPage
 {
+
 	public Login()
 	{
 		InitializeComponent();
 	}
+
+    private async void homePage(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MainPage());
+    }
 }
