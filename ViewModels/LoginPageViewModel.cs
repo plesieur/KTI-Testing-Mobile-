10096c2645b42e28dfc4f12fd3edc8710e059c92
Ineedsleep <Ineedsleep@DESKTOP-58M2OF2>
2024-02-28 11:58:41 -0500

    testing

--- ViewModels/LoginPageViewModel.cs
+++ ViewModels/LoginPageViewModel.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace KTI_Testing__Mobile_.ViewModels
 {
     public partial class LoginPageViewModel: BaseViewModel
