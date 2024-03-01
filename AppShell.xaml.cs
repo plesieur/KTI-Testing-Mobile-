35925ad37fb13d2a212f2ea2e6b52dca28e60d55
Anon U <Anon U@DESKTOP-I5PLASG>
2024-03-01 09:58:15 -0500

    fixed stuff

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,15 +1,12 @@
-<<<<<<< HEAD
-﻿using KTI_Testing__Mobile_;
+using KTI_Testing__Mobile_;
 
 namespace MauiApp2
 {
     public partial class AppShell : Shell
     {
-        
         public AppShell()
         {
             InitializeComponent();
-
             Routing.RegisterRoute(nameof(ProfilePage), typeof(ProfilePage));
             Routing.RegisterRoute(nameof(HistoryPage), typeof(HistoryPage));
             Routing.RegisterRoute(nameof(CartPage), typeof(CartPage));
@@ -18,24 +15,3 @@ namespace MauiApp2
         }
     }
 }
-=======
-﻿using KTI_Testing__Mobile_.Pages;
-
-namespace MauiApp2
-{
-    public partial class AppShell : Shell
-    {
-        
-        public AppShell()
-        {
-            InitializeComponent();
-
-            Routing.RegisterRoute(nameof(ProfilePage), typeof(ProfilePage));
-            Routing.RegisterRoute(nameof(HistoryPage), typeof(HistoryPage));
-            Routing.RegisterRoute(nameof(CartPage), typeof(CartPage));
-            Routing.RegisterRoute(nameof(SettingsPage), typeof(SettingsPage));
-            Routing.RegisterRoute(nameof(ToolsPage), typeof(ToolsPage));
-        }
-    }
-}
->>>>>>> 26e8b99429aadd2b322a4947d3ca7b0818675f0d
