cca22dfb46439b93a2aee0e336babcf0131a8f21
Antonio <aneto2024@jpkeefehs.org>
2024-03-14 10:20:35 -0400

    HTTP REQUEST

--- Models/UserInfo.cs
+++ Models/UserInfo.cs
@@ -8,11 +8,11 @@ namespace KTI_Testing__Mobile_.Models
 {
     public class UserInfo
     {
-        public int UsertId { get; set; }
+        public int UserId { get; set; }
 
         public string UserName { get; set; }
 
-        public string Passsword { get; set; }
+        public string Password { get; set; }
 
     }
 }
