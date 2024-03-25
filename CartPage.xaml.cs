27537d0538df7de6e082822a5f064071b8d6c870
Ineedsleep <Ineedsleep@DESKTOP-58M2OF2>
2024-03-25 12:38:55 -0400

    getting things to work

--- CartPage.xaml.cs
+++ CartPage.xaml.cs
@@ -5,40 +5,25 @@ using KTI_Testing__Mobile_.Models;
 
 namespace MauiApp2
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CartPage : ContentPage
     {
-        List<Tool> toolList = new List<Tool>();
 
         public CartPage()
         {
             InitializeComponent();
-
-            Tool clone2Tool = new Tool(1, "Hammer", "a bangy boi", "hehe", 50);
-
-            for (int i = 0; i < 20; i++)
-            {
-                AddItem(clone2Tool);
-            }
         }
-        private void AddItem(Tool tool)
+
+        private void GoCancle_Clicked(object sender, EventArgs e)
         {
-            toolList.Add(tool);
-
-            var myStyle = new Style<Entry>(
-
-            (Entry.HeightRequestProperty, 120),
-            (Entry.MaximumWidthRequestProperty, 430),
-            (Entry.ShadowProperty, 10),
-            (Entry.TextColorProperty, Colors.Black),
-            (Entry.BackgroundColorProperty, Colors.WhiteSmoke),
-            (Entry.FontSizeProperty, 28)
-            );
-
-            Button button = new Button { Text = tool.Name, Style = myStyle };
-            button.Clicked += (s, e) => { Navigation.PushAsync(new ToolsPage(tool)); };
-            button.Margin = new Thickness(15, 15, 15, 0);
-
+            OutputLabel.Text = "are you sure?";
+        }
+        private void Gosubmit_Clicked(object sender, EventArgs e)
+        {
+            OutputLabel.Text = "are you sure?";
+        }
+        private void GoToSettingPage(object sender, EventArgs e)
+        {
+            Shell.Current.GoToAsync(nameof(SettingsPage));
         }
     }
 }
