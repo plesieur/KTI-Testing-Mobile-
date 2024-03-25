ab6ff578fcd292a547a16bb2190ae6bee5cd4ba1
Ineedsleep <Ineedsleep@DESKTOP-58M2OF2>
2024-03-25 12:45:47 -0400

    fix it.

---
+++ temp.xaml.cs
@@ -0,0 +1,34 @@
+namespace MauiApp2;
+
+using CommunityToolkit.Maui.Markup;
+using KTI_Testing__Mobile_.Models;
+
+[QueryProperty("addedTool", "addedTool")]
+public partial class temp : ContentPage
+{
+    Tool addedTool;
+    public temp()
+    {
+        InitializeComponent();
+    }
+
+    public temp(Tool t)
+    {
+        InitializeComponent();
+
+        var myStyle = new Style<Entry>(
+
+        (Entry.HeightRequestProperty, 120),
+        (Entry.MaximumWidthRequestProperty, 430),
+        (Entry.ShadowProperty, 10),
+        (Entry.TextColorProperty, Colors.Black),
+        (Entry.BackgroundColorProperty, Colors.WhiteSmoke),
+        (Entry.FontSizeProperty, 28)
+        );
+
+        Button button = new Button { Text = t.Name, Style = myStyle };
+        button.Margin = new Thickness(15, 15, 15, 0);
+
+        Cart.Children.Add(button);
+    }
+}
