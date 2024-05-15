9b43aec0232b13271704d0a7b27f040b15b28052
Jerome <123493904+PastsEmpower@users.noreply.github.com>
2024-05-15 10:34:11 -0400

    Update and rename Tool.cs to Tools.cs

---
+++ Models/Tools.cs
@@ -0,0 +1,10 @@
+﻿namespace KTI_Testing__Mobile_.Models
+{
+    public class Tools
+    {
+        public int toolId { get; set; }
+        public string Name { get; set; }
+        public string Quantities { get; set; }
+        public int Quantity { get; set; }
+    }
+}
